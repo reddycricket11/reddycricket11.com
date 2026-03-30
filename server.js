7a576481a8bad1d8c9d4fd00859a786a26a414b8
reddycricket11 <alltypeindian@gmail.com>
2026-03-30 13:17:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -102,10 +102,10 @@ mongoose.connect(
   }
 );
 
-// cronjobs()
+ cronjobs()
  createDefaultContestTypes()
 // updateBalls();
-// addMatchtoDb();
+ addMatchtoDb();
 // addLiveDetails();
 // addLivePlayers();
 // addLivescoresDetails();
@@ -113,7 +113,7 @@ mongoose.connect(
 // addTeamstandingstodb();
 // addteamPlayers();
 // addTeamstandingstodbAPI();
-// addPlayersAPI();
+ addPlayersAPI();
 // startTransaction();
 // addLivecommentary();
 // addLivecommentaryCustom('odi')
@@ -123,7 +123,7 @@ mongoose.connect(
 //addInPlayStatus();
 // startCryptoTransaction();
 // updateSeries()
-// updateSquads()
+ updateSquads()
 // generateShotTypes()
 // fetchAndSaveTeams();
 // addMatchesForAllCurrentSeries()
