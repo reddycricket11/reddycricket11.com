ca746e95585c4331a5b142a5309033a47a406e78
reddycricket11 <alltypeindian@gmail.com>
2026-04-02 12:25:01 +0530

    Update createContestTypes.js

--- updating/createContestTypes.js
+++ updating/createContestTypes.js
@@ -2,54 +2,55 @@ const ContestType = require("../models/contestType");
 
 module.exports.createDefaultContestTypes = async function createDefaultContestTypes() {
     const defaultContestTypes = [
-      {
-        name: "Contest Type 1",
-        description: "Description for Contest Type 1",
-        prize: 17,
-        totalSpots: 2,
-        numWinners: 1,
-        entryFee: 10,
-        prizes: [
-          { rank: 1, amount: 17 },
-        ],
-      },
-      {
-        name: "Contest Type 2",
-        description: "Description for Contest Type 2",
-        prize: 17,
-        totalSpots: 2,
-        numWinners: 2,
-        entryFee: 10,
-        prizes: [
-            { rank: 1, amount: 18 },
-             ],
-        },
-        {
-          name: "Contest Type 3",
-          description: "Description for Contest Type 3",
-          prize: 17,
-          totalSpots: 2,
-          numWinners: 2,
-          entryFee: 10,
-          prizes: [
-            { rank: 1, amount: 5 },
-            { rank: 2, amount: 7 },
-          ],
-        },
-        {
-          name: "Contest Type 4",
-          description: "Description for Contest Type 4",
-          prize: 40,
-          totalSpots: 4,
-          numWinners: 3,
-          entryFee: 20,
-          prizes: [
-            { rank: 1, amount: 20 },
-            { rank: 2, amount: 12 },
-            { rank: 3, amount: 8 },
-          ],
-        },
-      ];
+const defaultContestTypes = [
+  {
+    name: "Contest Type 1",
+    description: "Description for Contest Type 1",
+    prize: 17,
+    totalSpots: 2,
+    numWinners: 1,
+    entryFee: 10,
+    prizeDetails: [
+      { rank: 1, prize: 17 }
+    ],
+  },
+  {
+    name: "Contest Type 2",
+    description: "Description for Contest Type 2",
+    prize: 17,
+    totalSpots: 2,
+    numWinners: 2,
+    entryFee: 10,
+    prizeDetails: [
+      { rank: 1, prize: 18 }
+    ],
+  },
+  {
+    name: "Contest Type 3",
+    description: "Description for Contest Type 3",
+    prize: 17,
+    totalSpots: 2,
+    numWinners: 2,
+    entryFee: 10,
+    prizeDetails: [
+      { rank: 1, prize: 5 },
+      { rank: 2, prize: 7 }
+    ],
+  },
+  {
+    name: "Contest Type 4",
+    description: "Description for Contest Type 4",
+    prize: 40,
+    totalSpots: 4,
+    numWinners: 3,
+    entryFee: 20,
+    prizeDetails: [
+      { rank: 1, prize: 20 },
+      { rank: 2, prize: 12 },
+      { rank: 3, prize: 8 }
+    ],
+  },
+];
       for (const contestType of defaultContestTypes) {
         const existingContestType = await ContestType.findOne({ name: contestType.name });
         if (!existingContestType) {
