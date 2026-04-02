78c075d9e3433b15ff2f4dd5eb59451b76821df8
reddycricket11 <alltypeindian@gmail.com>
2026-04-02 13:30:04 +0530

    Update createContestTypes.js

--- updating/createContestTypes.js
+++ updating/createContestTypes.js
@@ -16,45 +16,6 @@ module.exports.createDefaultContestTypes = async function createDefaultContestTy
           { rank: 3, amount: 0 },
         ],
       },
-      {
-        name: "Contest Type 2",
-        description: "Description for Contest Type 2",
-        prize: 2000,
-        totalSpots: 200,
-        numWinners: 20,
-        entryFee: 20,
-        prizes: [
-            { rank: 1, amount: 1000 },
-            { rank: 2, amount: 600 },
-            { rank: 3, amount: 400 },
-          ],
-        },
-        {
-          name: "Contest Type 3",
-          description: "Description for Contest Type 3",
-          prize: 3000,
-          totalSpots: 300,
-          numWinners: 30,
-          entryFee: 30,
-          prizes: [
-            { rank: 1, amount: 1500 },
-            { rank: 2, amount: 900 },
-            { rank: 3, amount: 600 },
-          ],
-        },
-        {
-          name: "Contest Type 4",
-          description: "Description for Contest Type 4",
-          prize: 4000,
-          totalSpots: 400,
-          numWinners: 40,
-          entryFee: 40,
-          prizes: [
-            { rank: 1, amount: 2000 },
-            { rank: 2, amount: 1200 },
-            { rank: 3, amount: 800 },
-          ],
-        },
       ];
       for (const contestType of defaultContestTypes) {
         const existingContestType = await ContestType.findOne({ name: contestType.name });
