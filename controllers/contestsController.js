1d20850064e4b7e44cf808a16e4b5491bb83d785
reddycricket11 <alltypeindian@gmail.com>
2026-04-02 14:46:58 +0530

    Update contestsController.js

--- controllers/contestsController.js
+++ controllers/contestsController.js
@@ -223,7 +223,7 @@ router.post("/createContestType", async (req, res) => {
 
       // Create new contest for this match
       const contest = new Contest({
-        price: req.body.prize,
+        price: contestType.prize,
         totalSpots: contestType.totalSpots,
         spotsLeft: contestType.totalSpots,
         matchId: match.matchId,
