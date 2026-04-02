cb6599db34d3b9818fa1a72885607d834962b4fa
reddycricket11 <alltypeindian@gmail.com>
2026-04-02 12:48:24 +0530

    Update contest.js

--- models/contest.js
+++ models/contest.js
@@ -6,6 +6,10 @@ const contestSchema = new mongoose.Schema(
       type: Number,
       required: true,
     },
+entryFee: {                 // ✅ ADD THIS
+      type: Number,
+      required: true,
+    },
 
     totalSpots: {
       type: Number,
