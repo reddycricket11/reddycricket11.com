6ba02e29caebd421a69e6d0f58c8e02d41161b5f
reddycricket11 <alltypeindian@gmail.com>
2026-04-04 14:04:57 +0530

    Update contestsController.js

--- controllers/contestsController.js
+++ controllers/contestsController.js
@@ -138,25 +138,6 @@ router.get("/joincontest/:id", async (req, res) => {
   if (date < match.date) {
   if (user.wallet >= contest.price / contest.totalSpots) {
       user.wallet -= (contest.price / contest.totalSpots);
-   // ✅ sirf wallet check (simple)
-if (user.wallet < entryFee) {
-  return res.status(400).json({
-    message: "Insufficient balance",
-    success: false,
-  });
-}
-
-// ✅ wallet deduct
-user.wallet -= entryFee;
-
-// ✅ IMPORTANT FIX (Amount Added bhi deduct hoga)
-if (user.totalAmountAdded) {
-  user.totalAmountAdded -= entryFee;
-
-  // ❗ negative hone se bachao
-  if (user.totalAmountAdded < 0) {
-    user.totalAmountAdded = 0;
-  }
       user.numberOfContestJoined = user.numberOfContestJoined + 1;
       contest.userIds.push(req.body.uidfromtoken);
       contest.teamsId.push(req.query.teamid);
